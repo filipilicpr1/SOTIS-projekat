ea464849530b40fe16b6e5abb84cf3f5f094c06e
Filip Ilic <filipflyer98@gmail.com>
2023-10-21 16:22:31 +0200

    Integrated services

--- main_service/services/new_course_services.py
+++ main_service/services/new_course_services.py
@@ -5,16 +5,10 @@ PYTHON_SERVICE_API_URI = config('PYTHON_SERVICE_API_URI')
 
 def upload_new_pdf_and_send_to_service(pdf_file):
     files = {
-    'data' : pdf_file
+        'file' : pdf_file
     }
 
-    headers = {
-    'Accept': "multipart/form-data",
-    'Content-Type': "application/pdf",
-    'Cache-Control': "no-cache",
-    }
-
-    r = requests.post(f'Server URL: {PYTHON_SERVICE_API_URI}', files=files, headers=headers)
+    r = requests.post(f'{PYTHON_SERVICE_API_URI}', files=files)
     
-    with open(pdf_file,"wb") as f:
-        f.write(r)
+    with open(f'test_name.pkl', "wb") as f:
+        f.write(r.content)
