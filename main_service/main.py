754074716ae978cd6ab305c11d0ce761703ee1b7
Filip Ilic <filipflyer98@gmail.com>
2023-10-22 15:27:16 +0200

    Renamed course controller

--- main_service/main.py
+++ main_service/main.py
@@ -1,10 +1,10 @@
 from flask import Flask
 from flask_cors import CORS
-from controllers import new_course_controller
+from controllers import course_controller
 
 app = Flask(__name__)
 CORS(app)
-app.register_blueprint(new_course_controller.bp)
+app.register_blueprint(course_controller.bp)
 
 if __name__ == "__main__":
     app.run(port=5001)
