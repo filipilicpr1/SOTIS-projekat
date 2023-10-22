7da5d1131fc588002114df8dd1c1eeb0a5ad3c17
Filip Ilic <filipflyer98@gmail.com>
2023-10-22 14:45:55 +0200

    Added main service endpoint for asking questions

--- main_service/services/new_course_services.py
+++ main_service/services/new_course_services.py
@@ -3,9 +3,14 @@ from decouple import config
 
 PYTHON_SERVICE_API_URI = config('PYTHON_SERVICE_API_URI')
 
-def upload_new_pdf_and_send_to_service(pdf_file):
+def upload_new_pdf_and_send_to_service(pdf_file, course_id):
     files = {
         'file' : pdf_file
     }
 
-    requests.post(f'{PYTHON_SERVICE_API_URI}/course/test_name', files=files)
+    requests.post(f'{PYTHON_SERVICE_API_URI}/documents/course/{course_id}', files=files)
+
+def get_answer_from_service(course_id, question):
+    response = requests.get(f'{PYTHON_SERVICE_API_URI}/chat/course/{course_id}?question={question}')
+    
+    return response.json, response.status_code
