49fa08b3379bdee1e519fc82ef681ffa85336c35
Filip Ilic <filipflyer98@gmail.com>
2023-10-22 15:05:55 +0200

    Fixed status code and not returning when doc is not found

--- integration_service/controller/chat_controller.py
+++ integration_service/controller/chat_controller.py
@@ -8,14 +8,15 @@ chat_bp = Blueprint('chat', __name__, url_prefix='/api/chat')
 @chat_bp.route('/course/<course_id>', methods=['GET'])
 def create_answer(course_id):
     document=get_document(course_id)
+    
     if document is None:
-        make_response(jsonify(UNDETECTED_COURSE_ERROR),400)
+        return make_response(jsonify(UNDETECTED_COURSE_ERROR),400)
         
     question=request.args.get('question')
     
     if question:
         answer=get_answer(document,question=question)
-        return make_response(jsonify(answer), 201)
+        return make_response(jsonify(answer), 200)
        
     return make_response(jsonify(ERROR_RESPONSE),400)
     
