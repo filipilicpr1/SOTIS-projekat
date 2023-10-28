9cd03f97f1c10cb8348f24a6c4a3db567113ac26
Filip Ilic <filipflyer98@gmail.com>
2023-10-28 19:02:10 +0200

    Added creating new course

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./userSlice";
+import courseReducer from "./courseSlice";
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
+    course: courseReducer
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
